7f06b2dcf3a904ec61838bcb9e295a0573ba567a
Aakash Kumar Goryan <aakashgoryan@gmail.com>
2018-04-14 03:19:22 +0530

    Added carouser to home page-incomplete

--- public/javascripts/Home.js
+++ public/javascripts/Home.js
@@ -24,3 +24,15 @@ $('.ui.dropdown')
 ;
 
 $('.masthead').css('min-height',$(window).height());
+
+new Glide('.glide', {
+    type: 'carousel',
+    autoplay: 2000,
+    hoverpause: false,
+    startAt: 0,
+    perView: 2,
+    gap: 10,
+    animationTimingFunc: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)',
+    animationDuration: 800
+}).mount();
+
