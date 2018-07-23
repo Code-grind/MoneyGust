2c974ace7b2ea05832466821d316b38ff41e2d6c
Reshu Verma <vreshu1998@gmail.com>
2018-07-24 01:16:26 +0530

    added moneygust account for online database

--- database/connect.js
+++ database/connect.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 ///const databaseName = 'moneygustdb';
 ///const url = 'mongodb://localhost/'+databaseName;
 ///mLab Database
-const url = 'mongodb://aakash:aakash1@ds247101.mlab.com:47101/moneygustdb';
+const url = 'mongodb://Admin:MoneyGust1@ds147411.mlab.com:47411/moneygustdb';
 
 function connect() {
-    mongoose.connect(url)
+    mongoose.connect(url,{ useNewUrlParser: true })
         .then(function (db) {
             console.log("database port: " + db['connections'][0]['port'])
         });
